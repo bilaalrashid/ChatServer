dc7764f6310abbe6e19b7f03d1fcf9dbf367690f
TheRealBilaal <42493384+TheRealBilaal@users.noreply.github.com>
2019-10-25 20:23:13 +0100

    Gets the port to run the server on

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+/**
+ * The main interface for the server
+ */
 public class Main {
 
+    /**
+     * Gets the port to run the server on
+     * @param args The command line arguments
+     */
     public static void main(String[] args) {
-	// write your code here
+        if (args != null && args.length == 1) {
+            int port = Integer.parseInt(args[0]);
+        } else {
+            System.out.println("Invalid arguments. Port not specified.");
+        }
     }
+
 }
